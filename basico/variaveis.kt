f453204f7e68465fce57f3e2edf5821e61c6ce8e
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-01 12:41:55 -0300

    variaveis e tipos de dados

--- basico/variaveis.kt
+++ basico/variaveis.kt
@@ -1,5 +1,33 @@
-fun main() {
-  val nome: String = "Marcelo"
+// var -> variavel mutavel
+// val -> variavel imutavel
+// const val -> variavel imutavel declaro fora da função
 
-  println(nome)
+/*
+  - TIPOS DE DADOS
+  String -> Textos
+  Double -> Números decimais, 16 numeros apos o ponto
+  Float -> numeros decimais, 7 numeros apos o ponto
+  Long -> Numeros Inteiros
+  Boolean -> Verdadeiro ou Falso
+*/
+
+const val sobrenome = "Santos"
+
+fun main() {
+  var idade = 29
+  val nome: String = "Marcelo"
+  val dinheiro = 25.99
+  val divida = 110f
+  val loteria: Long = 514484949
+  val resultado = false
+
+  //println(nome +" "+ sobrenome)
+  println("Meu nome é $nome $sobrenome")
+
+  println(idade)
+  println(dinheiro)
+  println(divida)
+  println(loteria)
+  println(resultado)
+  
 }
