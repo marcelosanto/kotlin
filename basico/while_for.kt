afdf3d9d5b82f380436af6f1b045b500d93f290a
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-05 12:42:34 -0300

    while e for

---
+++ basico/while_for.kt
@@ -0,0 +1,12 @@
+fun main() {
+  // var contador = 0
+
+  // while(contador < 10){
+  //   println("Contador: $contador")
+  //   contador++
+  // }
+
+  for(contador in 0..10){
+    println("Contador: $contador")
+  }
+}
