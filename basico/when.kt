ce2f9aeb507569da579a274fa3c15d1b2b8d6f70
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-05 13:26:34 -0300

    when

---
+++ basico/when.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+  // when(10<2){
+  //   true -> println("É verdadeira")
+  //   false -> println("É falsa")
+  // }
+
+  var opcao = 40
+
+  when(opcao) {
+    1 -> println("Suco de manga")
+    2 -> println("Suco de goiaba")
+    3 -> println("Suco de limao")
+    4 -> println("Suco de laranja")
+    5 -> println("Suco de acerola")
+    else -> println("Suco não encontrado")
+  } 
+}
