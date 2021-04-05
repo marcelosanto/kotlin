55e75a5bb11114b7bd4fcab8efd7d2df47340814
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-05 12:12:37 -0300

    operadores relacionais

---
+++ basico/operadores_relacionais.kt
@@ -0,0 +1,20 @@
+/*
+  -> (==) Operador de Igualdade
+  -> (<) Operador de Menor
+  -> (>) Operador de Maior
+  -> (<=) Operador de Menor igual
+  -> (>=) Operador de Maior igual
+  -> (!=) Operador de Diferente
+*/
+
+fun main() {
+  var valor1 = 10
+  var valor2 = 20
+
+  println(valor1 == valor2)
+  println(valor1 < valor2)
+  println(valor1 > valor2)
+  println(valor1 <= valor2)
+  println(valor1 >= valor2)
+  println(valor1 != valor2)
+}
