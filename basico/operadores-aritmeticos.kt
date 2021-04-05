181269bc6544bb669c55f39c58bc93b2b1ed37ba
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-05 12:04:16 -0300

    operadores aritmericos

---
+++ basico/operadores-aritmeticos.kt
@@ -0,0 +1,25 @@
+/*
+  -> (+) Operador de Soma
+  -> (-) Operador de Subtração
+  -> (/) Operador de Divisão
+  -> (*) Operador de Multiplicação
+  -> (%) Modulo - Resto da Divisão
+  -> (++) Incremento
+  -> (--) Decremento
+*/
+
+fun main() {
+  var numero1 = 100
+  var numero2 = 20
+
+  println(numero1 + numero2)
+  println(numero1 - numero2)
+  println(numero1 * numero2)
+  println(numero1 / numero2)
+  println(numero1 % numero2)
+  println(++numero1)
+  println(--numero2)
+
+  
+
+}
--- operadores_aritmeticos/main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-  val count = 10
-  val times = 9
-  val r = count / times
-  println(r)
-  println(count.div(times))
-}
