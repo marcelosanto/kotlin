4204f98240a48adfbb1d6ee359b02cf7621eb032
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-05 12:20:33 -0300

    if e else

---
+++ basico/if_else.kt
@@ -0,0 +1,12 @@
+fun main() {
+  var numero1 = 20
+  var numero2 = 20
+
+  if(numero1 > numero2){
+    println("É maior")
+  } else if (numero1 == numero2) {
+    println("É igual")
+  } else {
+    println("É menor")
+  }
+}
