a6e5eec18ed1764e385504571955009e6a84cf5e
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-06 14:43:21 -0300

    iniciando

--- orientacao_objetos/inicio.kt
+++ orientacao_objetos/inicio.kt
@@ -4,8 +4,29 @@ fun main() {
   carro.cor = "Azul"
   println(carro.cor)
 
+  carro.abrirPorta()
+  carro.marchas()
+
 }
 
 class Carro {
-  var cor: String = ""
+  var cor: String = "" // Atributo - Caracteristica do objeto
+
+  //Ações - Metodos
+  fun abrirPorta() {
+    println("Abrir a porta do carro")
+  }
+
+  fun primeraMarcha() {
+    println("Passando a primera marcha")
+  }
+
+  fun segundaMarcha() {
+    println("Passando a segunda marcha")
+  }
+
+  fun marchas() {
+    this.primeraMarcha()
+    this.segundaMarcha()
+  }
 }
